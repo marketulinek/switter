798c9df6e23962eb4a08a0ba21da2a296c91434a
Marketa <habranova.marketa@gmail.com>
2022-07-07 21:39:56 +0200

    Implemented a post-save signal

--- switter/models.py
+++ switter/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -13,3 +14,20 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+# ------------------------------
+#            SIGNALS
+# ------------------------------
+def create_profile(sender, instance, created, **kwargs):
+
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+        # newly created user automatically follows their own profile
+        user_profile.follows.add(instance.profile)
+        user_profile.save()
+
+# Create a Profile for each new user
+post_save.connect(create_profile, sender=User)
