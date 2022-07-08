eaa1fa2ffcc769c47abe6fe12d15e2b1e8a1396f
Marketa <habranova.marketa@gmail.com>
2022-07-08 22:04:29 +0200

    Refactoring signal using decorator

--- switter/models.py
+++ switter/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -19,6 +20,9 @@ class Profile(models.Model):
 # ------------------------------
 #            SIGNALS
 # ------------------------------
+
+# Create a Profile for each new user
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
 
     if created:
@@ -28,6 +32,3 @@ def create_profile(sender, instance, created, **kwargs):
         # newly created user automatically follows their own profile
         user_profile.follows.add(instance.profile)
         user_profile.save()
-
-# Create a Profile for each new user
-post_save.connect(create_profile, sender=User)
