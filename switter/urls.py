7dedb8e3c9140cb8736c32fb19f0df6f7172124c
Marketa <habranova.marketa@gmail.com>
2022-07-08 23:01:21 +0200

    Listed all user profiles in template

--- switter/urls.py
+++ switter/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, profile_list
 
 
 app_name='switter'
 
 urlpatterns = [
-    path('', dashboard, name='dashboard')
+    path('', dashboard, name='dashboard'),
+    path('profile-list/', profile_list, name='profile_list')
 ]
--- switter/views.py
+++ switter/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Profile
 
 
 def dashboard(request):
     return render(request, 'base.html')
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, 'switter/profile_list.html', {'profiles': profiles})
